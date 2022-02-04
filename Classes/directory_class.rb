949cf9d48cf42181cc64086129d07c89c8376e36
zuanshi88 <adwhitmer@gmail.com>
2022-02-04 11:46:25 -0600

    Directory.save_update working well, now changing it to save and creating a top level save_update in HQ

--- Classes/directory_class.rb
+++ Classes/directory_class.rb
@@ -9,6 +9,9 @@ class Directory
 
   #instance variable called by all instances of directories to provide
   #up-to-date touch points 
+
+  #pull instance variable out and pass it to Touchpoints to produce all
+  # of its data. Directory can ask for the touch points from touch_points. 
   def create_tps
     tps = []
       @accounts.each do |entry|
@@ -19,6 +22,23 @@ class Directory
         end
       end
       return tps
+    end
+
+       # here we are connecting directory to head_quarters using the 
+       # head_quarters @database instance variable which connects
+       #this instance of directory with this instance of headquarters. 
+       #here we are using a global method to encapsulate calls to 
+       #instance methods
+    def self.save_update(database:, database_file:, delete: false, entry:)
+          database.delete_account(entry) 
+            if delete == false 
+               database.add_account(entry)
+            end 
+          Directory.marshal_save(database.accounts, database_file)
+    end
+
+    def self.marshal_save(obj_array, file)
+        File.open(file, "wb"){|f| f.write(Marshal.dump(obj_array))}
     end
 
 
