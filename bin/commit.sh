9eb5acfd14db713899e41df6e9d84325fb1d9dc3
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 10:26:39 -0600

    testing after getting testing set back up

--- bin/commit.sh
+++ bin/commit.sh
@@ -12,3 +12,5 @@ git commit -m "$RESPONSE"
 # bash ./Specs/bin/run_unit_test.sh
 # bash ./Specs/bin/run_integration_test.sh
 bash ./Specs/bin/hello_world.sh
+bash ./Specs/bin/run_unit_test.sh
+bash ./Specs/bin/run_unit_test.sh
