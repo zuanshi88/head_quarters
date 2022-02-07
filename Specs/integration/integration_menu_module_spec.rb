34ed36835d8c859d6bf03876554e184f64d7b0cf
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 12:01:15 -0600

    testing that the accounts size is correct

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -74,11 +74,11 @@ class TestAddressBookIntegration < Test::Unit::TestCase
 
      def test_address_can_delete_and_save_update 
         size = @address_book.session.database.accounts.size
-        assert_equal(size, @address_book.session.database.accounts.size)
-        @address_book.session.save_update(@entry, delete: false)
-        assert_equal(size, @address_book.session.database.accounts.size)
-        @address_book.session.save_update(@entry, delete: true)
-        assert_equal(size, @address_book.session.database.accounts.size)
+        assert_equal(90 @address_book.session.database.accounts.size)
+        # @address_book.session.save_update(@entry, delete: false)
+        # assert_equal(size, @address_book.session.database.accounts.size)
+        # @address_book.session.save_update(@entry, delete: true)
+        # assert_equal(size, @address_book.session.database.accounts.size)
      end 
 
     # def test_display_method 
