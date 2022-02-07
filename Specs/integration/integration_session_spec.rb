233458e27dee1bbc9aac23a226fff8ad507d471c
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 16:32:39 -0600

    refactoring save_update method in Directory class

--- Specs/integration/integration_session_spec.rb
+++ Specs/integration/integration_session_spec.rb
@@ -31,7 +31,7 @@ class TestSession < Test::Unit::TestCase
         @session.save_update(@entry, delete = true)
         assert_equal(size, @session.database.accounts.size)
         entry = @session.database.accounts[10]
-        assert_equal("Damn Anderson", @session.database.accounts[10].name)
+        assert_equal("Spring Anderson", @session.database.accounts[10].name)
     end
 end 
 
