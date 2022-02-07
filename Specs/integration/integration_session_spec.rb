d9f3c6f7faf3333e28e2c76d6bf65adba2c8e9d7
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 16:52:54 -0600

    aborting the revert

--- Specs/integration/integration_session_spec.rb
+++ Specs/integration/integration_session_spec.rb
@@ -27,8 +27,8 @@ class TestSession < Test::Unit::TestCase
         size = @session.database.accounts.size
         entry = @session.database.accounts[10]
         assert_equal(Entry, entry.class)
-        assert_equal("Spring Anderson", entry.name)
-        @session.save_update(entry: @entry, delete: true)
+          assert_equal("Spring Anderson", entry.name)
+        @session.save_update(@entry, delete: true)
         assert_equal(size - 1, @session.database.accounts.size)
         entry = @session.database.accounts[10]
         assert_equal("Spring Anderson", @session.database.accounts[10].name)
