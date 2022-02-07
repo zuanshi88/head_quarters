e051f3dbdb14abc83b81da440ab5131bc4ff5e18
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 12:44:19 -0600

    breadking delete test to make sure it is registering

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -78,7 +78,7 @@ class TestAddressBookIntegration < Test::Unit::TestCase
         @address_book.session.save_update(@entry)
         assert_equal(size + 1, @address_book.session.database.accounts.size)
         @address_book.session.save_update(@entry, delete: true)
-        assert_equal(size, @address_book.session.database.accounts.size)
+        assert_equal(size + 10, @address_book.session.database.accounts.size)
      end 
 
     # def test_display_method 
