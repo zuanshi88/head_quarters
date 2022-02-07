10bd45176569caf0b63a697a2b6ad7f2b0d99252
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 10:41:27 -0600

    no need to leading backslash when you call from same directory and you are giving dir/file orientation

--- Specs/bin/run_integration_test.sh
+++ Specs/bin/run_integration_test.sh
@@ -2,8 +2,8 @@
 
 echo "Hello World"
 
-ruby /integration/integration_directory_spec.rb 
-ruby /integration/integration_menu_module_spec.rb
+ruby integration/integration_directory_spec.rb 
+ruby integration/integration_menu_module_spec.rb
 # ruby ./head_quarters_spec.rb 
 # ruby ./integration/integration_touch_point_and_entry_spec.rb
 # ruby ./integration/integration_head_quarters_spec.rb
