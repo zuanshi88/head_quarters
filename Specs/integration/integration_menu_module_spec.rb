3c0e32d624b954f40200936db58be65dc427ef44
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 11:11:37 -0600

    fixed erroneous FILE in integration_menu_module, assigned database in integration spec

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -10,8 +10,11 @@ class TestAddressBookIntegration < Test::Unit::TestCase
     
     
     def setup
-        FILE = 
-        @address_book = Address_Book.new()
+          #we are loading our data from the @read_file
+        @read_file = 'c_read_test_database.txt'
+        #we are saving the results of our test runs to the @write file
+        @write_file = 'write_test_database.txt'
+        @address_book = Address_Book.new(@read_file)
         @entry = Entry.new({"first name" => "Aaron", "last name" => "Whitmer", "email" => "adwhitmer@gmail.com", "phone number" =>"(773) 673-0803"})
         @entry_hash = {"first name" => "Zaron", "last name" => "Zitmer", "email" => "zwhitmer@gmail.com", "phone number" =>"(333) 333-3833"}
     end
