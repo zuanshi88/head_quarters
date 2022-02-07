d6f749b8a19c2584456bbb7c9b32d64a083777b3
zuanshi88 <adwhitmer@gmail.com>
2022-02-07 10:38:46 -0600

    Finally have this working again!

--- Specs/bin/commit.sh
+++ Specs/bin/commit.sh
@@ -3,14 +3,11 @@
 
 git add .
 
-echo 'What?'
+echo 'What would you like to record? -- make it good.'
 
 read RESPONSE
 
 git commit -m "$RESPONSE"
 
-# bash ./Specs/bin/run_unit_test.sh
-# bash ./Specs/bin/run_integration_test.sh
-bash ./Specs/bin/hello_world.sh
-# bash ./Specs/bin/run_unit_test.sh
-# bash ./Specs/bin/run_unit_test.sh
+bash ./bin/run_unit_test.sh
+bash ./bin/run_integration_test.sh
