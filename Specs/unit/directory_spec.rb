dd7b5906465b238989168e68fbd3cbca4858c995
zuanshi88 <adwhitmer@gmail.com>
2022-02-08 14:32:49 -0600

    now testing save update both adding and deleting

--- Specs/unit/directory_spec.rb
+++ Specs/unit/directory_spec.rb
@@ -100,11 +100,13 @@ class TestDirectory < Test::Unit::TestCase
     end 
 
     def test_class_method_save_update 
-     @touch_point_entry =  Entry.new({"first name" => "Stan", "last name" => "Razny", "email" => "adwhitmer@gmail.com", "phone number" =>"(773) 673-0803"})
-     Directory.create_touch_point(@touch_point_entry, Time.now, "Did some real good deals.")
-     size = @directory.accounts.size 
-     Directory.save_update(database: @directory, database_file: @directory.database_file, entry: @touch_point_entry, delete: false)
-    assert_equal(size, @directory.accounts.size)
+        @touch_point_entry =  Entry.new({"first name" => "Stan", "last name" => "Razny", "email" => "adwhitmer@gmail.com", "phone number" =>"(773) 673-0803"})
+        Directory.create_touch_point(@touch_point_entry, Time.now, "Did some real good deals.")
+        size = @directory.accounts.size 
+        Directory.save_update(database: @directory, database_file: @directory.database_file, entry: @touch_point_entry, delete: false)
+        assert_equal(size + 1, @directory.accounts.size)
+        Directory.save_update(database: @directory, database_file: @directory.database_file, entry: @touch_point_entry, delete: true)
+         assert_equal(size, @directory.accounts.size)
     end 
 
     # def test_touch_point_length 
