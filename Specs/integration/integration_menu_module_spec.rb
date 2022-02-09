95449a8f290a6a6ffc790447d10caa12fa1a518a
zuanshi88 <adwhitmer@gmail.com>
2022-02-09 14:00:36 -0600

    int_menu_mod --test_session_can_delete_touch_point_at_both_entry_and_database_levels -- brough refresh database back

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -114,11 +114,11 @@ class TestAddressBookIntegration < Test::Unit::TestCase
             total_size = @session.database.touch_points.size
             @session.database.create_touch_point(@entry, Time.now, "Playing SUPER FUN games") 
             @session.database.save_update(@entry)
+            @session.refresh_database_instance
             assert_equal(size + 1, @entry.touch_points.size)
             assert_equal(total_size + 1, @session.database.touch_points.size)
             @entry.touch_points.delete_at(0)
             @session.database.save_update(@entry)
-            # @session.refresh_database_instance
           
             assert_equal(size, @entry.touch_points.size)
             assert_equal(total_size - 1, @session.database.touch_points.size)
