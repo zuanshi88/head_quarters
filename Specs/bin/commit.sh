79990c57715514cbb3e3aa931a0a4ca288a8ff9b
zuanshi88 <adwhitmer@gmail.com>
2022-02-09 10:41:09 -0600

    is this broken?

--- Specs/bin/commit.sh
+++ Specs/bin/commit.sh
@@ -2,9 +2,7 @@
 
 echo ""
 
-stage=$(git add . )
-
-stage 
+git add .
 
 echo 'What would you like to record? -- make it good.'
 
@@ -12,5 +10,10 @@ read RESPONSE
 
 git commit -m "$RESPONSE"
 
+
+echo "UNIT TESTS COMMENCING"
 bash ./bin/run_unit_test.sh
+echo "UNIT TESTS COMPLETE"
+echo "INTEGRATION TESTS COMMENCING"
 bash ./bin/run_integration_test.sh
+echo "INTEGRATION TESTS COMPETE"
