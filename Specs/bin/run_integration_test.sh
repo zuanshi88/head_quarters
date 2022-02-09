fd7eef719ebabba112d44c7e2da1f733092e546e
zuanshi88 <adwhitmer@gmail.com>
2022-02-08 22:59:40 -0600

    no running session integration test as well

--- Specs/bin/run_integration_test.sh
+++ Specs/bin/run_integration_test.sh
@@ -5,7 +5,7 @@ echo "Hello World"
 ruby integration/integration_directory_spec.rb 
 
 # ruby integration/integration_menu_module_spec.rb
-# ruby ./integration/integration_session_spec.rb
+ruby ./integration/integration_session_spec.rb
 
 # ruby ./integration/integration_touch_point_and_entry_spec.rb
 
