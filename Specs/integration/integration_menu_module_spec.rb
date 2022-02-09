90fe70504a81fe41a6cac5c703e36346d6d2d287
zuanshi88 <adwhitmer@gmail.com>
2022-02-09 17:37:42 -0600

    post delete still working?

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -132,8 +132,8 @@ class TestAddressBookIntegration < Test::Unit::TestCase
 
             #could we pull the @entry out of the new database 
 
-            @session.database.accounts.delete_if{|i| i.name == @entry.name}
-              @session.database.marshal_save
+            # @session.database.accounts.delete_if{|i| i.name == @entry.name}
+            #   @session.database.marshal_save
             updated_entry = @session.database.accounts.select{|i| i.name == @entry.name }
             
 
