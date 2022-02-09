6f799bbab8f2e993082102118a01f155124e607d
zuanshi88 <adwhitmer@gmail.com>
2022-02-09 13:23:48 -0600

    found the BUG in the test, didn't include size in the expectation

--- Specs/integration/integration_menu_module_spec.rb
+++ Specs/integration/integration_menu_module_spec.rb
@@ -98,7 +98,7 @@ class TestAddressBookIntegration < Test::Unit::TestCase
 
         def test_session_can_delete_touch_point_at_both_entry_and_database_levels
             size = @entry.touch_points.size 
-            total_size = @session.database.touch_points 
+            total_size = @session.database.touch_points.size
             @session.database.create_touch_point(@entry, Time.now, "Playing SUPER FUN games") 
             @session.database.save_update(@entry)
             assert_equal(size + 1, @entry.touch_points.size)
