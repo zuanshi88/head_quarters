405359332b286336a11c46dd87fb43fdad2992cb
zuanshi88 <adwhitmer@gmail.com>
2022-02-09 21:25:52 -0600

    unleashing a neglected spec -- ./integration/integration_touch_point_and_entry_spec.rb

--- Specs/bin/run_integration_test.sh
+++ Specs/bin/run_integration_test.sh
@@ -7,6 +7,6 @@ ruby integration/integration_directory_spec.rb
 ruby integration/integration_menu_module_spec.rb
 ruby ./integration/integration_session_spec.rb
 
-# ruby ./integration/integration_touch_point_and_entry_spec.rb
+ruby ./integration/integration_touch_point_and_entry_spec.rb
 
 echo "Goodbye World"
