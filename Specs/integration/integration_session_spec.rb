94e957154ff19251ab3dcdd7923fb327888883dd
zuanshi88 <adwhitmer@gmail.com>
2022-02-10 11:12:10 -0600

    same same typo

--- Specs/integration/integration_session_spec.rb
+++ Specs/integration/integration_session_spec.rb
@@ -58,7 +58,7 @@ class TestSession < Test::Unit::TestCase
         assert_equal(1, entry_size )
         @session.database.delete_touch_point(@entry, @entry.touch_points[0])
         num_of_accounts = @session.database.accounts.size 
-        num_of_tps - @session.database.touch_points.size
+        num_of_tps = @session.database.touch_points.size
         assert_equal(entry_size - 1, @entry.touch_points.size )
         @session.refresh_database_instance
         assert_equal(num_of_tps, @session.database.touch_points.size )
