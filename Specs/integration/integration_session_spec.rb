6c203650dbe463f02654db032d5f1a1a972a3595
zuanshi88 <adwhitmer@gmail.com>
2022-02-10 11:17:40 -0600

    integration_session_spec --test_delete_touch_point -- # of accounts and # of tps before and after delete

--- Specs/integration/integration_session_spec.rb
+++ Specs/integration/integration_session_spec.rb
@@ -56,9 +56,9 @@ class TestSession < Test::Unit::TestCase
 
         entry_size = @entry.touch_points.size
         assert_equal(1, entry_size )
-        @session.database.delete_touch_point(@entry, @entry.touch_points[0])
         num_of_accounts = @session.database.accounts.size 
         num_of_tps = @session.database.touch_points.size
+        @session.database.delete_touch_point(@entry, @entry.touch_points[0])
         assert_equal(entry_size - 1, @entry.touch_points.size )
         @session.refresh_database_instance
         assert_equal(num_of_tps, @session.database.touch_points.size )
