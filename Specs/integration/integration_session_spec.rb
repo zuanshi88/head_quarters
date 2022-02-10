d8d62324a824e95f2dbfb06bcb8fdf7cc16ffd49
zuanshi88 <adwhitmer@gmail.com>
2022-02-10 12:17:59 -0600

    int_sess_test --test_create_touch_point -- why isn't it adding a touch_point exploring @entry

--- Specs/integration/integration_session_spec.rb
+++ Specs/integration/integration_session_spec.rb
@@ -35,6 +35,8 @@ class TestSession < Test::Unit::TestCase
     end 
 
     def test_create_touch_point 
+        entry_size = @entry.touch_points.size 
+        assert_equal(entry_size, 111)
         size = @session.database.touch_points.size
         @session.database.create_touch_point(@entry, Time.now, "Computing is the shit")
         @session.refresh_database_instance
